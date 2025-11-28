59e524e829a55fdae8e4c1e98e188bbb401a62d1
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-11-28 15:32:34 +0100

    virus4012

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`,
+        destination: `${process.env.BACKEND_URL || 'https://panel-1-tlqv.onrender.com'}/api/:path*`,
       },
     ];
   },

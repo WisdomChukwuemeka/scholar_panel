ffbb7590c65e01c2ad135fc2e7cdb1b224e4f41f
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-11-28 13:53:25 +0100

    virus401

--- app/services/api.js
+++ app/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Your backend URL
 // const BASE_URL = 'http://localhost:8000/api'; 
 const BASE_URL =  process.env.NEXT_PUBLIC_BASE_URL
-    ? `${process.env.NEXT_PUBLIC_BASE_URL}/api`
+    ? `${process.env.NEXT_PUBLIC_BASE_URL}`
     : "http://localhost:8000/api";
 
 const api = axios.create({
